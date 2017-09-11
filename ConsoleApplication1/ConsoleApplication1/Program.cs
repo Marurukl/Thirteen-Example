d2b7363800fea2e92ffdcfbe4970c7726960d274
lRaika <arnurelnur@gmail.com>
2017-09-12 00:03:39 +0600

    first change
    Finaly change

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace FirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1.В файле записана непустая последовательность целых чисел, являющихся числами Фибоначчи.
+            //Приписать еще столько же чисел этой последовательности.
+            string path = @"C:\NewLocation\fibonaccyNumbers";
+
+
+
+
+
+
+        }
+    }
+}
