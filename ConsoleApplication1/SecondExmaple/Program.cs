fe5a6683589985f5b1720a7cb13200e9722f0c22
lRaika <arnurelnur@gmail.com>
2017-09-12 12:23:04 +0600

    second change
    Finaly

--- ConsoleApplication1/SecondExmaple/Program.cs
+++ ConsoleApplication1/SecondExmaple/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace SecondExmaple
 {
@@ -10,6 +12,41 @@ namespace SecondExmaple
     {
         static void Main(string[] args)
         {
+            /*
+ 2.	Сложить два целых числа А и В.
+Входные данные
+В единственной строке входного файла INPUT.TXT записано два натуральных числа через пробел.
+Выходные данные
+В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.
+     */
+            int a, b;
+            string[] strNum;
+            string pathDirectory = @"C:\FibonaccyNumbers\";
+            Directory.CreateDirectory(pathDirectory);
+            using(FileStream fs = new FileStream(pathDirectory + "INPUT.TXT" , FileMode.Create))
+            {
+                string strStream = "1 2";
+                
+                byte[] bytes = new byte[strStream.Length];
+                bytes = Encoding.Default.GetBytes(strStream);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+            using (FileStream fs = new FileStream(pathDirectory+"INPUT.TXT", FileMode.Open))
+            {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, bytes.Length);
+                strNum = (Encoding.Default.GetString(bytes)).Split(' ');
+                Int32.TryParse(strNum[0], out a);
+                Int32.TryParse(strNum[1], out b);
+            }
+            using (FileStream fs = new FileStream(pathDirectory+"OUTPUT.TXT", FileMode.Create))
+            {
+                string sum = String.Format("{0}", a + b);
+                byte[] bytes = new byte[sum.Length];
+                bytes = Encoding.Default.GetBytes(sum);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+            Console.ReadLine();
         }
     }
 }
