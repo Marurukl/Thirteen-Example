d44d90b93c5ddbc72981860cd3f3ec5357278c9d
lRaika <arnurelnur@gmail.com>
2017-09-12 12:01:30 +0600

    first change

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,13 +14,84 @@ namespace FirstExample
         {
             //            1.В файле записана непустая последовательность целых чисел, являющихся числами Фибоначчи.
             //Приписать еще столько же чисел этой последовательности.
-            string path = @"C:\NewLocation\fibonaccyNumbers";
+            string pathDirectory = @"C:\FibonaccyNumbers\";
+            string path = pathDirectory + @"Fibonaccy.txt";
+            int size = 5;
+            int newSize = 0;
+            int first = 1;
+            int second = 1;
+            int sum = 0;
+            int[] firstFibonaccyNum = new int[size];
+            string[] strNum;
+
+            Directory.CreateDirectory(pathDirectory);
+
+            for (int i = 0 + newSize; i < size; i++)
+            {
+                firstFibonaccyNum[i] = first;
+                sum = first + second;
+                first = second;
+                second = sum;
+                Console.Write("{0} ", firstFibonaccyNum[i]);
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+
+                string strStream = "";
+                for (int i = 0; i < firstFibonaccyNum.Length; i++)
+                {
+                    strStream += firstFibonaccyNum[i].ToString();
+                    if (i < (firstFibonaccyNum.Length - 1)) strStream += " ";
+                }
+                byte[] bytes = new byte[strStream.Length];
+                bytes = Encoding.Default.GetBytes(strStream);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, bytes.Length);
+                strNum = (Encoding.Default.GetString(bytes)).Split(' ');
+                newSize = bytes.Length;
+            }
+
+            int sizeForArr = newSize + size;
+            Array.Resize(ref firstFibonaccyNum, sizeForArr);
+
+            for (int i = 0 + newSize; i < sizeForArr; i++)
+            {
+                firstFibonaccyNum[i] = first;
+                sum = first + second;
+                first = second;
+                second = sum;
+                Console.Write("{0} ", firstFibonaccyNum[i]);
+            }
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+
+                string strStream = "";
+                for (int i = 0; i < firstFibonaccyNum.Length; i++)
+                {
+                    strStream += firstFibonaccyNum[i].ToString();
+                    if (i < (firstFibonaccyNum.Length - 1)) strStream += " ";
+                }
+                byte[] bytes = new byte[strStream.Length];
+                bytes = Encoding.Default.GetBytes(strStream);
+                fs.Write(bytes, 0, bytes.Length);
+            }
 
 
 
 
 
 
+
+            Console.ReadLine();
         }
+
+        
     }
 }
